2c7eb19575c1bd2699f8d5ad857822c312da03b5
kalim_ullah <kalim.ullah@arpatech.com>
2020-03-16 16:54:14 +0500

    initail commit

---
+++ src/containers/App.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="site-header sticky-top py-1">
+        <div className="container d-flex flex-column flex-md-row justify-content-between">
+          <a className="py-2" href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
+          </a>
+          <a className="py-2 d-none d-md-inline-block" href="#">Tour</a>
+          <a className="py-2 d-none d-md-inline-block" href="#">Product</a>
+          <a className="py-2 d-none d-md-inline-block" href="#">Features</a>
+          <a className="py-2 d-none d-md-inline-block" href="#">Enterprise</a>
+          <a className="py-2 d-none d-md-inline-block" href="#">Support</a>
+          <a className="py-2 d-none d-md-inline-block" href="#">Pricing</a>
+          <a className="py-2 d-none d-md-inline-block" href="#">Cart</a>
+        </div>
+      </nav>
+
+
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>This is body</h1>
+            <p>The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. 
+Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid Line 75:19:  The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. 
+Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <footer className="container py-5">
+        <div className="row">
+          <div className="col-12 col-md">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="d-block mb-2"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
+            <small className="d-block mb-3 text-muted">&copy; 2017-2018</small>
+          </div>
+          <div className="col-6 col-md">
+            <h5>Features</h5>
+            <ul className="list-unstyled text-small">
+              <li><a className="text-muted" href="#">Cool stuff</a></li>
+              <li><a className="text-muted" href="#">Random feature</a></li>
+              <li><a className="text-muted" href="#">Team feature</a></li>
+              <li><a className="text-muted" href="#">Stuff for developers</a></li>
+              <li><a className="text-muted" href="#">Another one</a></li>
+              <li><a className="text-muted" href="#">Last time</a></li>
+            </ul>
+          </div>
+          <div className="col-6 col-md">
+            <h5>Resources</h5>
+            <ul className="list-unstyled text-small">
+              <li><a className="text-muted" href="#">Resource</a></li>
+              <li><a className="text-muted" href="#">Resource name</a></li>
+              <li><a className="text-muted" href="#">Another resource</a></li>
+              <li><a className="text-muted" href="#">Final resource</a></li>
+            </ul>
+          </div>
+          <div className="col-6 col-md">
+            <h5>Resources</h5>
+            <ul className="list-unstyled text-small">
+              <li><a className="text-muted" href="#">Business</a></li>
+              <li><a className="text-muted" href="#">Education</a></li>
+              <li><a className="text-muted" href="#">Government</a></li>
+              <li><a className="text-muted" href="#">Gaming</a></li>
+            </ul>
+          </div>
+          <div className="col-6 col-md">
+            <h5>About</h5>
+            <ul className="list-unstyled text-small">
+              <li><a className="text-muted" href="#">Team</a></li>
+              <li><a className="text-muted" href="#">Locations</a></li>
+              <li><a className="text-muted" href="#">Privacy</a></li>
+              <li><a className="text-muted" href="#">Terms</a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
